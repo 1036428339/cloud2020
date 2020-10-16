7f53de2a5f2418fdc9dd0791f9e67056b353b4a0
1036428339 <1036428339@qq.com>
2020-10-16 18:07:40 +0800

    添加Hystrix实现服务降级、服务熔断、Hystrix Dashboard监控

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/gtguigu/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,18 @@
+package com.gtguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author lwc
+ * @date 2020/10/16 15:54
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001
+{
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
